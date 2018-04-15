1303e0239aee6bf0499d1fa6eb6973b3178316f6
Apollo Tang <tang.apollo@gmail.com>
2018-04-15 11:00:53 -0400

    add some es6 to see it works

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import App from './App'
+
+const b = {b:'b'}
+class Test {
+  constructor(x) {
+    console.log('hello', x)
+  }
+  static p = {
+    ...b
+  }
+
+  m = () => {
+    console.log('this is m()')
+    console.log('this is p', Test.p)
+    console.log('this is this', this)
+  }
+}
+
+const t = new Test(1)
+t.m()
